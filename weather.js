da27114f35dc9ddc9c499142dd21a5091d84ae5d
income3000 <income3000@gmail.com>
2021-09-19 18:34:25 -0400

    2nd commit

--- weather.js
+++ weather.js
@@ -6,12 +6,14 @@ const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?units=imperial
 const form = document.querySelector("#weather");
 const zipCode = document.querySelector("#zipcode");
 const results = document.querySelector("#weather-result");
-
+const cityR = document.querySelector("#name")
 
 
 const updateResults = (data) => {
   const weatherHTML = `<h1 class="${data.main.temp}">Current Temp: ${data.main.temp}</h1>`;
   results.innerHTML = weatherHTML;
+  const cityHTML = `<h3 class="${data.name}">City: ${data.name}</h3>`;
+  cityR.innerHTML = cityHTML;
 };
 
 const handleSubmit = async (event) => {
@@ -20,6 +22,7 @@ const handleSubmit = async (event) => {
   const REQUEST_URL = BASE_URL + `&zip=${searchString},us`;
   const response = await fetch(REQUEST_URL);
   const weatherData = await response.json();
+  console.log(weatherData)
   updateResults(weatherData);
   
 };
